a8066a1dda6010d414673373f35ea64d9fc16468
DESKTOP-R2NKGM0\admin <zino1187@gmail.com>
2020-10-28 09:48:26 +0900

    c

--- SeProject/src/day1027/gui/ChatClient.java
+++ SeProject/src/day1027/gui/ChatClient.java
@@ -45,7 +45,7 @@ public class ChatClient extends JFrame implements KeyListener{
 		t_input.setPreferredSize(new Dimension(285,30));
 		
 		//보여주기 전에 미리 연결해놓자 (컴포넌트와 리스너 연결 )
-		bt.addActionListener(new MyActionListener());
+		bt.addActionListener(new MyActionListener(t_input, area));
 		t_input.addKeyListener(this);//프레임이 곧 리스너다!
 		
 		setSize(300,400);
