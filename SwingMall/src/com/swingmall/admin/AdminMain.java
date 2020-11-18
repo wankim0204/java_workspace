4a36271cbdf41205f99572cd365c1dabec6e1185
DESKTOP-R2NKGM0\admin <zino1187@gmail.com>
2020-11-18 14:28:50 +0900

    c

--- SwingMall/src/com/swingmall/admin/AdminMain.java
+++ SwingMall/src/com/swingmall/admin/AdminMain.java
@@ -32,7 +32,8 @@ public class AdminMain extends JFrame{
 	public static final int BOARD=4;//게시판관리
 		
 	JPanel user_container;//관리자,사용자 화면을 구분지을수 있는 컨테이너
-
+	JPanel p_content; //여기에 모든 페이지가 미리 붙어져있을것임
+								//추후 showPage 메서드로 보일지여부 설정..
 	JPanel p_navi; //웹사이트의 주 메뉴를 포함할 컨테이너 패널
 	
 	String[] navi_title= {"Home","상품관리","회원정보","주문관리","게시판관리"};
@@ -49,6 +50,7 @@ public class AdminMain extends JFrame{
 	public AdminMain() {
 		dbManager = new DBManager();
 		user_container = new JPanel();
+		p_content = new JPanel();
 		p_navi = new JPanel();
 		la_footer = new JLabel("SwingMall All rights reserved", SwingConstants.CENTER);
 		
@@ -84,12 +86,19 @@ public class AdminMain extends JFrame{
 		
 		//조립 
 		user_container.setLayout(new BorderLayout());
-		user_container.add(p_navi, BorderLayout.NORTH);
+
+		//모든 페이지를 미리 붙여놓자!!
+		for(int i=0;i<page.length;i++) {
+			p_content.add(page[i]);
+		}
+		showPage(AdminMain.PRODUCT);//처음에 나와야하는 페이지 설정
 		
-		user_container.add(page[AdminMain.PRODUCT]);//센터에 페이지 부착
+		user_container.add(p_navi, BorderLayout.NORTH);
+		user_container.add(p_content);//센터에 페이지 부착
 		
 		this.add(user_container);
 		this.add(la_footer, BorderLayout.SOUTH);
+		
 		
 		setSize(1200,900);
 		setVisible(true);
@@ -101,6 +110,36 @@ public class AdminMain extends JFrame{
 				dbManager.disConnect(con);
 			}
 		});
+		
+		//네비게이션 버튼과 리스너연결 
+		for(int i=0;i<navi.length;i++) {
+			navi[i].addActionListener((e)->{
+				Object obj = e.getSource();
+				if(obj==navi[0]) { //home
+					showPage(0);
+				}else if(obj==navi[1]) {
+					showPage(1);
+				}else if(obj==navi[2]) {
+					showPage(2);
+				}else if(obj==navi[3]) {
+					showPage(3);
+				}else if(obj==navi[4]) {
+					showPage(4);
+				}
+			});
+		}
+		
+	}
+	
+	//보여질 페이지와 않보여질 페이지를 설정하는 메서드 
+	public void showPage(int showIndex) { //매개변수로는 보여주고 싶은 페이지 넘버
+		for(int i=0;i<page.length;i++) {//모든 페이지를 대상으로..
+			if(i==showIndex) {
+				page[i].setVisible(true); //보이게할 페이지
+			}else {
+				page[i].setVisible(false); //않보이게할 페이지
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
