20bd72ecde2ef4612686332dfa9832f9c24bbd94
DESKTOP-R2NKGM0\admin <zino1187@gmail.com>
2020-11-20 09:48:41 +0900

    c

--- SwingMall/src/com/swingmall/member/RegistForm.java
+++ SwingMall/src/com/swingmall/member/RegistForm.java
@@ -2,8 +2,12 @@ package com.swingmall.member;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
@@ -50,10 +54,78 @@ public class RegistForm extends Page{
 		p_content.add(bt_regist);
 		
 		add(p_content);
-		
+	
+		bt_regist.addActionListener((e)->{
+			
+			if( checkId(t_mid.getText()) ) {
+				JOptionPane.showMessageDialog(RegistForm.this, "중복된 아이디입니다/n다른 아이디를 사용하세요");
+			}else {
+				regist(vo); // vo 값을 제대로 넣어 완성하기 10시5분 수업시작..
+			}
+			
+		});
 	}
 	
+	//회원존재여부 체크 
+	//반환값이true가 나오면, 회원가입 진행시키면 안됨!
+	public boolean checkId(String mid) {
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		boolean flag=false;
+		
+		String sql="select * from shop_member where mid=?";//여러분들이  sql 문 작성하기!! 회원테이블에 중복된 아이디가 있는지 여부에 대한 쿼리..
+		
+		try {
+			pstmt=getShopMain().getCon().prepareStatement(sql);
+			pstmt.setString(1, mid);
+			rs=pstmt.executeQuery();
+			flag=rs.next(); //레코드가 존재하면 true, 아니면 false 대입됨..
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			getShopMain().getDbManager().close(pstmt, rs);
+		}
+		return flag;
+	}
+	
+	//회원 등록
+	public int regist(ShopMember shopMember) {
+		//멤버변수 자동 초기화와는달리, 메서드의 지역변수는 컴파일러가 초기화하지 않으므로, 반드시 개발자가 초기화해야 한다..
+		PreparedStatement pstmt=null;
+		int result=0;
+		
+		String sql="insert into shop_member(member_id,mid,pass,name,phone,email)";
+		sql+=" values(seq_shop_member.nextval,?,?,?,?,?)";
+		
+		try {
+			pstmt=getShopMain().getCon().prepareStatement(sql);
+			//바인드변수 대입 
+			pstmt.setString(1, shopMember.getMid());
+			pstmt.setString(2, shopMember.getPass());
+			pstmt.setString(3, shopMember.getName());
+			pstmt.setString(4, shopMember.getPhone());
+			pstmt.setString(5, shopMember.getEmail());
+			result = pstmt.executeUpdate();//쿼리 수행 
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			getShopMain().getDbManager().close(pstmt);
+		}
+		return result;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
